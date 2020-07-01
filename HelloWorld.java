3dded411cf45f2c1ec86daf42e08992e4a131e5c
Kavya-Bangalore <67687089+Kavya-Bangalore@users.noreply.github.com>
2020-07-01 16:06:46 +0530

    Created HelloWorld java file
    It displays "Hello World" and "The first program executed".

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//Program:Printing Hello World 
+public class PrintHelloWorld{
+ public static void main(String args[]){
+  System.out.println("Hello World!");
+  System.out.println("The first program executed.");
+ }
+}
+ 
+ /*Output:Hello World!
+The first program executed.*/
